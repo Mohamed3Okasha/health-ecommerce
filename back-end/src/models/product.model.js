2ca8f1640fcf0d9c8bb6e307640b180e4825719d
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2022-06-04 16:07:19 +0200

    Removed unused refrences in product model

--- back-end/src/models/product.model.js
+++ back-end/src/models/product.model.js
@@ -17,17 +17,7 @@ const productSchema = new mongoose.Schema({
     },
     images: [
         String
-    ],
-    categories: [
-        {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Category'
-        }
-    ],
-    brand: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Brand' 
-    } 
+    ]
 }, 
 {
     timestamps: true
