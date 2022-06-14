c1bda354c655f950d7f3a6e03406adf93c7e5f6b
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2022-06-14 17:12:14 +0200

    Added address endpoint

--- back-end/src/controller/user.controller.js
+++ back-end/src/controller/user.controller.js
@@ -1,7 +1,8 @@
 const userModel = require("../models/user.model");
 const cartModel = require("../models/cart.model");
 const orderModel = require("../models/order.model");
-const productModel = require("../models/product.model")
+const productModel = require("../models/product.model");
+const addressModel = require("../models/address.model");
 const { sendWelcomeEmail } = require("../emails/account");
 
 class User {
@@ -169,6 +170,61 @@ class User {
       res.status(400).send(e.message);
     }
   }
+
+  static showAddresses = async (req, res) => {
+    try {
+      const addresses = await addressModel.find({user_id: req.user._id});
+      res.send(addresses);
+    }
+    catch (e) {
+      res.status(400).send(e.message);
+    }
+  }
+
+  static addAddress = async (req, res) => {
+    try {
+      const address = new addressModel({user_id:req.user._id, ...req.body});
+      await address.save();
+      res.send(address);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  };
+
+  static updateAddress = async (req, res) => {
+    try {
+      const address = await addressModel.findOneAndUpdate(
+        {user_id: req.user._id, _id: req.params.id},
+        req.body,
+        {
+          returnDocument: "after",
+        }
+      );
+      if (!address) {
+        res.status(404).send("Address not found");
+      }
+      else {
+        res.send(address);
+      }
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  };
+
+  static deleteAddress = async (req, res) => {
+    try {
+      const address = await addressModel.findOneAndDelete(
+          {user_id: req.user._id, _id: req.params.id});
+      if (!address) {
+        res.status(404).send("Address not found");
+      }
+      else {
+        res.send(address);
+      }
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  };
 }
 
 module.exports = User;
